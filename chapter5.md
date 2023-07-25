7114f0c0d28516daeab21075326180550cd93a5d
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-26 04:32:31 +0900

    Finish layout and routes

---
+++ chapter5.md
@@ -0,0 +1,6 @@
+mv kitten.jpg app/assets/images/
+
+<%= link_to image_tag "kitten.jpg" %>
+
+<%#= link_to image_tag "kitten.jpg" %>
+
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,36 +17,36 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe '#help' do
     it '正常にレスポンスを返すこと' do
-      get static_pages_help_path
+      get help_path
       expect(response).to have_http_status :ok
     end
 
     it 'Help | Ruby on Rails Tutorial Sample Appが含まれること' do
-      get static_pages_help_path
+      get help_path
       expect(response.body).to include "Help | #{base_title}"
     end
   end
 
   describe '#about' do
     it '正常にレスポンスを返すこと' do
-      get static_pages_about_path
+      get about_path
       expect(response).to have_http_status :ok
     end
 
     it 'About | Ruby on Rails Tutorial Sample Appが含まれること' do
-      get static_pages_about_path
+      get about_path
       expect(response.body).to include "About | #{base_title}"
     end
   end
 
   describe "#contact" do
     it "正常にレスポンスを返すこと" do
-      get static_pages_contact_path
+      get contact_path
       expect(response).to have_http_status :ok
     end
 
     it 'Contact | Ruby on Rails Tutorial Sample Appが含まれること' do
-      get static_pages_contact_path
+      get contact_path
       expect(response.body).to include "Contact | #{base_title}"
     end
   end
